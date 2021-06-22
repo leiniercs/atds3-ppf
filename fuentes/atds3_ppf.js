ffa55e29ca43f2fd1fee39b6f938ee8347b103de
Leinier Cruz Salfran <leiniercs@gmail.com>
2021-06-21 22:09:01 -0400

    Corrección en la inicialización de la base de datos.

--- fuentes/atds3_ppf.js
+++ fuentes/atds3_ppf.js
@@ -1,28 +1,30 @@
-const sqlite3 = require('sqlite3');
-const baseDatos = new sqlite3.Database('atds3_ppf.db');
 const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 80 });
+const sqlite3 = require('sqlite3');
+let baseDatos = new sqlite3.Database('atds3_ppf.db');
 let temporizadorInvalidacionFichas;
 
 
 function iniciarBaseDatos() {
 	baseDatos.exec("PRAGMA page_size = 8192");
-	baseDatos.exec("PRAGMA schema.cache_size = -32768");
 	baseDatos.exec("PRAGMA temp_store = MEMORY");
 	baseDatos.exec("PRAGMA journal_mode = OFF");
 	baseDatos.exec("PRAGMA locking_mode = EXCLUSIVE");
 	baseDatos.exec("PRAGMA synchronous = OFF");
 	
-	baseDatos.exec("CREATE TABLE fichas (id INTEGER PRIMARY KEY AUTOINCREMENT, ficha TEXT NOT NULL, expiracion INTEGER NOT NULL)");
-	baseDatos.exec("CREATE UNIQUE INDEX uniq_fichas_ficha ON fichas (ficha)");
+	baseDatos.exec("CREATE TABLE IF NOT EXISTS fichas (id INTEGER PRIMARY KEY AUTOINCREMENT, ficha TEXT NOT NULL, expiracion INTEGER NOT NULL)");
+	baseDatos.exec("CREATE UNIQUE INDEX IF NOT EXISTS uniq_fichas_ficha ON fichas (ficha)");
+	
+	temporizadorInvalidacionFichas = setInterval(eventoInvalidacacionFichas, 60000);
 }
 
-async function comandoAportarFicha(mensaje) {
+function comandoAportarFicha(mensaje) {
 	if (mensaje["ficha"] !== undefined && mensaje["expiracion"] !== undefined) {
 		baseDatos.exec(`INSERT INTO fichas (ficha, expiracion) VALUES ('${mensaje.ficha}', ${mensaje.expiracion})`);
 	}
 }
 
-async function comandoObtenerFicha(socalo) {
+function comandoObtenerFicha(socalo) {
 	let resultados = baseDatos.exec("SELECT ficha FROM fichas ORDER BY random() LIMIT 1");
 	
 	socalo.send({ accion: "entregarFicha", ficha: resultados["ficha"]});
@@ -33,7 +35,8 @@ function eventoInvalidacacionFichas() {
 	baseDatos.exec("VACUUM");
 }
 
-async function eventoConexion(socalo) {
+function eventoConexion(socalo) {
+	console.debug("eventoConexion()");
 	socalo.on("message", function eventoMensaje(mensaje) {
 		if (mensaje.accion === "aportarFicha") {
 			comandoAportarFicha(mensaje);
@@ -46,12 +49,5 @@ async function eventoConexion(socalo) {
 
 
 iniciarBaseDatos();
-temporizadorInvalidacionFichas = setInterval(eventoInvalidacacionFichas, 60000);
-
-const wss = new WebSocket.Server({
-	port: 443,
-	perMessageDeflate: false,
-	clientTracking: true
-});
 
 wss.on("connection", eventoConexion);
