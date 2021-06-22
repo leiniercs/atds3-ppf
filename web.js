3994b9d6ed968179d0465ee383e35b4c67b62eaa
Leinier Cruz Salfran <leiniercs@gmail.com>
2021-06-22 01:03:19 -0400

    Corrección menor.

--- web.js
+++ web.js
@@ -1,18 +1,12 @@
 const process = require('process');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.PORT });
+const wss = new WebSocket.Server({ port: parseInt(process.env.PORT) });
 const sqlite3 = require('sqlite3');
 let baseDatos = new sqlite3.Database('atds3_ppf.db');
 let temporizadorInvalidacionFichas;
 
 
 function iniciarBaseDatos() {
-	baseDatos.exec("PRAGMA page_size = 8192");
-	baseDatos.exec("PRAGMA temp_store = MEMORY");
-	baseDatos.exec("PRAGMA journal_mode = OFF");
-	baseDatos.exec("PRAGMA locking_mode = EXCLUSIVE");
-	baseDatos.exec("PRAGMA synchronous = OFF");
-	
 	baseDatos.exec("CREATE TABLE IF NOT EXISTS fichas (id INTEGER PRIMARY KEY AUTOINCREMENT, ficha TEXT NOT NULL, expiracion INTEGER NOT NULL)");
 	baseDatos.exec("CREATE UNIQUE INDEX IF NOT EXISTS uniq_fichas_ficha ON fichas (ficha)");
 	
@@ -25,7 +19,7 @@ function comandoAportarFicha(mensaje) {
 	} catch (_e) {}
 }
 
-function comandoObtenerFicha(socalo) {
+function comandoSolicitarrFicha(socalo) {
 	let resultados = baseDatos.exec("SELECT ficha FROM fichas ORDER BY random() LIMIT 1");
 	
 	socalo.send({ accion: "entregarFicha", ficha: resultados["ficha"]});
@@ -41,11 +35,11 @@ function eventoConexion(socalo) {
 
 	socalo.on("message", function eventoMensaje(mensaje) {
 		try {
-			if (mensaje.accion === "aportarFicha") {
+			if (mensaje.accion === 'aportarFicha') {
 				comandoAportarFicha(mensaje);
 			}
-			if (mensaje.accion === "obtenerFicha") {
-				comandoObtenerFicha(socalo);
+			if (mensaje.accion === 'solicitarrFicha') {
+				comandoSolicitarrFicha(socalo);
 			}
 		} catch (_e) {}
 	});
